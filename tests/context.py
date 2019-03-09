c1fba94572aeae26d9625997e13998fcfbf23dc6
eduardojdiniz <eduardojdiniz@gmail.com>
2019-03-09 01:13:20 -0500

    add comments to 'context.py' and fixed relative import erros in 'test_*.py'

--- tests/context.py
+++ tests/context.py
@@ -1,7 +1,32 @@
 # -*- coding: utf-8 -*-
 
+# context.py provide runtime context for importing the module "sample"
 import sys
 import os
+
+# Because python checks in the directories in sequenctial order, starting at
+# the first directory in sys.path list, till it find the .py file it is looking
+# for, it may inadvertly import a module instead of a package with the same
+# name. For instance:
+# - test
+#    - shared
+#       - __init__.py
+#       - phtest.py
+#    - testmain.py
+#    - shared.py
+# the interpreter will raise an error if I try from shared import phtest from
+# testmain.py: ImportError : cannot import name 'phtest'. This happens because
+# it first search in the current directory and tries to get 'phtest' from
+# shared.py instead of getting the module phtest inside ./shared.py
+# To correct that we are going to put in sys.path at position '0' (first
+# directory it will search for) the path to our module package to give the test
+# package proper import context
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
+# Then, withing the individual test modules, import the module like so:
+# from context import sample. By doing this, whomever is importing this module
+# with get sample in their namespace. If I were to run just import context, I
+# would have to use context.sample to acess the module 'sample'
+# This works because python runs modules as it get imported. It just do not
+# execute code below "if __name__ == "__main__"
 import sample
--- tests/test_advanced.py
+++ tests/test_advanced.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from .context import sample
+from context import sample
 
 import unittest
 
@@ -9,6 +9,11 @@ class AdvancedTestSuite(unittest.TestCase):
     """Advanced test cases."""
 
     def test_thoughts(self):
+        # important to remember that '__init__' of pkg 'sample' actually
+        # imported the 'hmm' method from 'core.py', otherwise it would not work
+        # because 'context.py' module has only 'sample' in its namespace, but
+        # does not have anly of the modules in 'sample' into its local
+        # namespace
         self.assertIsNone(sample.hmm())
 
 
